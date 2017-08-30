65ee4a7544a8d4c47522310faf7b41a1610ce621
kleinsche <744197047@qq.com>
2017-08-30 16:54:51 +0800

    撞色

--- calayer/ViewController.swift
+++ calayer/ViewController.swift
@@ -12,14 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let img = UIImageView()
+        img.frame = self.view.frame
+        img.image = #imageLiteral(resourceName: "house3")
+        view.addSubview(img)
+        
+        let gradientLayer = CAGradientLayer.init()
+        gradientLayer.frame = img.frame
+        gradientLayer.colors = [UIColor.gray.cgColor,UIColor.clear.cgColor,UIColor.clear.cgColor,UIColor.gray.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.locations = [0.1,0.3,0.7,0.9]
+        
+        img.layer.addSublayer(gradientLayer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
